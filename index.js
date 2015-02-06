fd3753321a5ce00d50867f2389e7752f948dfd9f
David Wood <david.p.wood@gmail.com>
2015-02-06 16:26:03 +0000

    Switched to using express.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-var api = require('./lib/api')();
+var express = require('express');
+var socketio = require('socket.io');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
-var port = process.env.PORT || 3000;
+function handler(io, req, res) {
+  var payload = req.body.payload;
+  
+  console.log('got push payload: ', payload);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end();
 
-api.listen(port);
+  io.emit('payload', payload);
+}
+
+var app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var server = require('http').Server(app);
+var io = socketio(server);
+
+app.post('/payload', handler.bind(app, io));
+
+io.on('connection', function (socket) {
+  console.log('connection event');
+});
+
+server.listen(process.env.PORT || 3000);
